ec4117369a9df9e932b9c0f44be628a80c37cc9a
Bhawani <parajulibhawani@gmail.com>
2016-08-29 11:38:02 -0700

    add geolocation using ip address

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -5,19 +5,46 @@ export default Ember.Component.extend({
   didInsertElement: function(mainMap){
     var container = this.$('.map-display')[0];
     var options = {
-      center: {lat: -34.397, lng: 150.644},
+      center: {lat: 45.5231, lng: 122.6765},
       zoom: 8
     };
     var map = this.get('map').findMap(container, options);
-      window.google.maps.event.addListener(map, 'click', function(event) {
-        addMarker(event.latLng, map);
-      });
-    function addMarker(location, map) {
-      var marker = new google.maps.Marker({
-        position: location,
-        map: map
-      });
-    console.log(marker.position.lat());
-    }
-  }
-});
+      // window.google.maps.event.addListener(map, 'click', function(event) {
+      //   addMarker(event.latLng, map);
+      // });
+      var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      //   infoWindow.setPosition(pos);
+      //   infoWindow.setContent(browserHasGeolocation ?
+      //                         'Error: The Geolocation service failed.' :
+      //                         'Error: Your browser doesn\'t support geolocation.');
+      // }
+    // function addMarker(location, map) {
+    //   var marker = new google.maps.Marker({
+    //     position: location,
+    //     map: map
+    //   });
+    // console.log(marker.position.lat());
+    // }
+  });
