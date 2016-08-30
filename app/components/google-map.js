d9b8fd79681ba2d103b001eae895eb0189e0c401
Bhawani Parajuli and Jim Klein <bhawani+jim@>
2016-08-30 16:20:55 -0700

    additional position work.

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -2,40 +2,37 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   map: Ember.inject.service('google-map'),
-
-    drawMap: Ember.computed('map.pos', function() {
-      if(this.get('map').pos) {
-        console.log("in drawMap computed property");
-        console.log(this.get('map').pos);
-        var container = this.$('.map-display')[0];
-        var options = {
-          center: {lat: 37.0902, lng: 95.7129},
-          zoom: 15
-        };
-        var map = this.get('map').findMap(container, options);
-        window.google.maps.event.addListener(map, 'click', function(event) {
-          addMarker(event.latLng, map);
-
+  didInsertElement: function(mainMap){
+    var container = this.$('.map-display')[0];
+    var options = {
+      center: {lat: 37.0902, lng: 95.7129},
+      zoom: 15
+    };
+    var map = this.get('map').findMap(container, options);
+      window.google.maps.event.addListener(map, 'click', function(event) {
+        addMarker(event.latLng, map);
+      });
+      function addMarker(location, map) {
+        var marker = new google.maps.Marker({
+          position: location,
+          map: map
         });
-        function addMarker(location, map) {
-          var marker = new google.maps.Marker({
-            position: location,
-            map: map
-          });
-          var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+marker.position.lat()+','+marker.position.lng()+'&radius=50&type=brewery&key=AIzaSyDttrvfo7P6LseYqJztA_M5bYTm4sQaReY';
-          return Ember.$.getJSON(url).then(function(responseJSON){
+        var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+marker.position.lat()+','+marker.position.lng()+'&radius=50&type=brewery&key=AIzaSyDttrvfo7P6LseYqJztA_M5bYTm4sQaReY';
+    return Ember.$.getJSON(url).then(function(responseJSON){
+      console.log(marker.position.lat);
+      console.log(responseJSON.results);
+      console.log(responseJSON.results);
+      return responseJSON.results;
+    });
+  }
 
-            console.log(responseJSON.results);
-            return responseJSON.results;
-          });
-        }
-        var infoWindow = new google.maps.InfoWindow({map: map});
-        var pos = this.get('map').pos
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        map.setCenter(pos);
-      }
-    }),
+  var infoWindow = new google.maps.InfoWindow({map: map});
+  console.log(this.get('map'));
+    var pos = this.get('pos');
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    map.setCenter(pos);
+},
     actions: {
       debug() {
         debugger;
